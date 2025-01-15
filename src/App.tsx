bf0268fccd56a79903ff4436f199418a3364f226
rychardaniel <164958686+rychardaniel@users.noreply.github.com>
2025-01-15 17:50:23 -0300

    testando deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import ComponentTest from './components/ComponentTest'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <h3>Em breve será uma calculadora...</h3>
+      <ComponentTest />
       <button onClick={() => setCount((count) => count + 1)}>
         count is {count}
       </button>
---
+++ src/components/ComponentTest.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ComponentTest = () => {
+  return (
+    <div style={{marginBottom: "20px"}}>Sou um componente teste</div>
+  )
+}
+
+export default ComponentTest
